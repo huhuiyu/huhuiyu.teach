7c26228b37697939a2ea0a4a39e1d18cdf17cc48
Administrator <Administrator@paul-vm-vs.localdomain>
2017-06-14 21:57:02 +0800

    完成了多文件上传和图片文件显示功能

--- AspDotNet/AspMvc/FileUpload/Controllers/DefaultController.cs
+++ AspDotNet/AspMvc/FileUpload/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
 ﻿using FileUpload.dal.dao;
+using FileUpload.dal.entity;
 using FileUpload.Models;
 using System;
 using System.Collections.Generic;
@@ -80,6 +81,61 @@ namespace FileUpload.Controllers
             //文件路径，文件类型，下载文件名称
             return File(filePath, model.UpFile.ContentType, model.UpFile.Filename);
         }
+
+        public ActionResult ShowImage(DefaultModel model)
+        {
+            return View(model);
+        }
+
+        public ActionResult ToUpFiles(DefaultModel model)
+        {
+            return View(model);
+        }
+
+        public ActionResult AddFiles(DefaultModel model, HttpPostedFileBase[] uploadFile)
+        {
+            try
+            {
+                //处理文件上传
+                if (uploadFile != null)
+                {
+                    foreach (HttpPostedFileBase uf in uploadFile)
+                    {
+                        TblFiles tf = new TblFiles();
+                        tf.Description = model.UpFile.Description;
+
+                        //原始文件名称
+                        string filename = uf.FileName;
+                        if (filename.IndexOf("\\") > -1)
+                        {
+                            filename = filename.Substring(filename.LastIndexOf("\\") + 1);
+                        }
+                        tf.Filename = filename;
+                        //通过guid产生不重复的文件名
+                        tf.Filepath = Guid.NewGuid().ToString();
+                        //获取文件类型和文件大小
+                        tf.ContentType = uf.ContentType;
+                        tf.Size = uf.ContentLength;
+                        //设置保存文件名
+                        string saveName = Server.MapPath(
+                            WebMvc.MvcApplication.UploadPath +
+                            tf.Filepath);
+                        //保存文件
+                        uf.SaveAs(saveName);
+                        //保存信息到数据库
+                        TblFilesDAO.Add(tf);
+                    }
+                }
+                model.ActionCode = 200;
+            }
+            catch (Exception ex)
+            {
+                model.ActionCode = 500;
+                model.ActionMessage = ex.Message;
+            }
+            return View("ToUpFiles", model);
+        }
+
 
     }
 }
